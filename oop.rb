b06c01b17ba063ca3214f3468b50e110b1909e80
J Seymour <JustJakeSeymour@gmail.com>
2022-09-05 12:42:06 -0600

    Initial commit, halfway through oop.rb

--- oop.rb
+++ oop.rb
@@ -1,25 +1,72 @@
 # Write a class called Unicorn
-# it should have a dynamic name attribute
-# it should have a color attribute, that is silver by default
-# it should have a method called "say" that returns whatever string is passed in, with "*~*" at the beginning and end of the string
+class Unicorn
+  def initialize(name)
+    # it should have a dynamic name attribute
+    @name = name
+    # it should have a color attribute, that is silver by default
+    @color = "silver"
+  end
 
+  # it should have a method called "say" that returns whatever string is passed in, with "*~*" at the beginning and end of the string
+  def say()
+    print "What does #{@name} say?"
+    words = gets
+    print "#{@name} says, *~*#{words}."
+  end
+end
 
 
 #  Write a class called Vampire
-#  it should have a dynamic name attribute
-#  it should have a pet attribute, that is a bat, by default BUT it could be dynamic if info is passed in initially
-#  it should have a thirsty attribute, that is true by default
-#  it should have a drink method. When called, the thirsty attribute changes to false
+class Vampire
+  def initialize(name)
+    #  it should have a dynamic name attribute
+    @name = name
+    #  it should have a pet attribute, that is a bat, by default
+    #  BUT it could be dynamic if info is passed in initially
+    @pet = "bat"
+    #  it should have a thirsty attribute, that is true by default
+    @thirsty = true
+  end
+
+  # it should have a method called "say" that returns whatever string is passed in, with "*~*" at the beginning and end of the string
+  def change_pet(dif_pet)
+    @pet = dif_pet
+  end
+  #  it should have a drink method. When called, the thirsty attribute changes to false
+  def drink()
+    @thirsty = false
+  end
+end
 
 
 #  Write a Dragon class
-#  it should have a dynamic name attribute (string)
-#  it should have a dynamic rider attribute (string)
-#  it should have a dynamic color attribute (string)
-#  it should have a is_hungry attribute that is true by default
+class Dragon
+  def initialize(name, rider, color)
+    #  it should have a dynamic name attribute (string)
+    @name = name
+    #  it should have a dynamic rider attribute (string)
+    @rider = rider
+    #  it should have a dynamic color attribute (string)
+    @color = color
+    #  it should have a is_hungry attribute that is true by default
+    @is_hungry = true
+  end
+
 #  it should have a eat method. If the dragon eats 4 times, it is no longer hungry
+  def eat
+  end
 
+count = 0
+name = :eat
 
+  TracePoint.trace(:call) do |t|
+  count += 1 if t.method_id == name
+  end
+
+  if count >= 4
+    then @diet = false
+  end
+end
 #  Write a Hobbit class
 #  it should have a dynamic name attribute (string)
 #  it should have a dynamic disposition attribute (string)
